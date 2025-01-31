c73a966c45fdce75520d466fe0ddb5c4409d2bbe
Haya-chb <haya.chaibi@hotmail.com>
2025-01-31 14:22:03 +0100

    Add-files

---
+++ SAE105-HayaChaibi-TPA/data.js
@@ -0,0 +1,47 @@
+/*Niveau 1
+var musiques =["Kiss my wound", "Hind's hall","Family Line", "From Eden"];
+
+var descriptionsMusiques = ["Description Kiss my wound","Description Hind's Hall","Description Family Line", "Description From Eden"];*/
+
+var musique
+var descriptionMusicale
+
+var data = [
+
+    {
+        "musique": "C'est la vie",
+        "descriptionMusicale": "Cette musique incarne, à mes yeux, un classique de la musique franco-arabe. On la retrouvait partout, notamment dans les mariages. Elle a marqué mon enfance, ses notes  aujourd'hui résonnent avec nostalgie.",
+        "url": "Musiques/C'est la vie-.mp4",
+        "image": "Images/telephone-Khaled.png",
+        "Youtube": "https://youtu.be/5dWeeUIZFgA?si=ci4lOilIfVvsjLgS"
+
+    },
+
+    {
+        "musique": "Hind's Hall",
+        "descriptionMusicale": "À travers cette musique, porteuse d’un message puissant, une solidarité envers la Palestine prend vie, mais aussi envers tous ceux qui luttent contre l’oppression. Elle agit comme un rappel des valeurs essentielles et des luttes qui méritent d’être menées ",
+        "url": "Musiques/Hind's hall.mp4",
+        "image": "Images/telephone-macklemore.png",
+        "Youtube": "https://youtu.be/fgDQyFeBBIo?si=C7SdZ10LEX6WwHoA"
+
+    },
+
+    {
+        "musique": "Zina",
+        "descriptionMusicale": "Reprise par de nombreux artistes, cette musique est un classique de la culture arabe. Connue par tous, elle éblouit par sa beauté et sa richesse.",
+        "url": "Musiques/Zina.mp4",
+        "image": "Images/telephone-zina.png",
+        "Youtube": "https://youtu.be/zKGW9V1WSl8?si=L9HLJbTHZaG2P4UN"
+    },
+
+    {
+        "musique": "Bengarden",
+        "descriptionMusicale": "Cet été, cette chanson a marqué les esprits en rendant hommage à Bengarden, une petite ville du sud de la Tunisie, d'où ma famille est originaire. C'est la première fois, à ma connaissance, qu'une musique célèbre si fièrement notre ville. On l’a entendue dans tous les mariages, créant une ambiance mémorable et remplie de fierté.",
+        "url": "Musiques/Bengarden.mp4",
+        "image": "Images/telephone-bengarden.png",
+        "Youtube": "https://youtu.be/fxcIVefT0WI?si=Ub08FLyDYIzQufkK"
+
+    },
+]
+
+
